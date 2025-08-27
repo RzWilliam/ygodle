eb04c1d81c59dc87bea788e83d159cb2bf8dc814
RzWilliam <williamrodriguez0621@gmail.com>
2025-08-27 16:08:51 +0200

    🔥 Remove unused files

--- src/components/DailyCardInitializer.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { useDailyCardInitializer } from '../hooks/useDailyCardInitializer';
-
-// Composant pour gérer l'initialisation
-export const DailyCardInitializer: React.FC = () => {
-  useDailyCardInitializer();
-  return null; // Ce composant ne rend rien
-};
